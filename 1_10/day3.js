a43de331695e103f7707d8c5b355e30110e68d75
Hbeom00 <bum6968@naver.com>
2023-09-18 19:46:13 +0900

    create(Hbeom00) 2023-09-18 : 문제1번 ~ 문제 12번

---
+++ 1_10/day3.js
@@ -0,0 +1,12 @@
+/*
+문제3.변수의 타입
+다음 출력 값으로 올바른 것은?
+let arr = [100, 200, 300];
+console.log(typeof(arr));
+1) undefined 2) string 3) number 4) object
+*/
+
+let arr = [100, 200, 300];
+console.log(typeof arr);
+
+/* 정답: 4번 => 배열이므로 object */
