36ef65ecabf9d2adc0c741c789361342b04daee5
Hbeom00 <bum6968@naver.com>
2023-09-21 17:13:05 +0900

    feat(Hbeom00) 2023-09-21 : add 21번~24번 풀이

---
+++ 21_30/day21.js
@@ -0,0 +1,14 @@
+/*
+문재21. set은 어떻게 만드나요?
+다음 중 set을 만드는 방법으로 올바른 것을 모두 고르시오.
+
+1)  let x = {1, 2, 3, 5, 6, 7};
+2)  let x = {};
+3)  let x = new Set('javascript');
+4)  let x = new Set(range(5));
+5)  let x = new Set();
+*/
+
+/* 정답: 3번, 5번 */
+let x = new Set("javascript");
+console.log(x);
