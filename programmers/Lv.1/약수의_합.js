62cb213965dd8b20cea3bc3eba04f766c0977f6c
Hbeom00 <bum6968@naver.com>
2024-11-10 23:22:56 +0900

    Lv.1 약수의 합 풀이

---
+++ programmers/Lv.1/약수의_합.js
@@ -0,0 +1,9 @@
+function solution(n) {
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      result.push(i);
+    }
+  }
+  return result.reduce((sum, el) => sum + el, 0);
+}
