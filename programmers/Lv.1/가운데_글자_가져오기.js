84171ac99b947b655edb5fe9cac060d7c329a29f
Hbeom00 <bum6968@naver.com>
2024-12-29 23:22:27 +0900

    Lv.1 가운데 글자 가져오기 풀이

---
+++ programmers/Lv.1/가운데_글자_가져오기.js
@@ -0,0 +1,5 @@
+function solution(s) {
+  return s.length % 2 === 1
+    ? s.substring(Math.floor(s.length / 2), Math.floor(s.length / 2) + 1)
+    : s.substring(s.length / 2 - 1, s.length / 2 + 1);
+}
